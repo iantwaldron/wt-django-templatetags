5b9e4bcce5c9d4ca5f97ba1f00fbd67a27c95457
Ian Waldron <ian@waldrontech.com>
2025-09-17 14:42:28 -0700

    Add templatetags dir; add pagination tags.

---
+++ wt_tools/templatetags/wt_pagination_tags.py
@@ -0,0 +1,40 @@
+from typing import Union
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def relative_url(
+        value: Union[int, str],
+        field_name: str,
+        urlencode: str = None
+) -> str:
+    """
+    Create a relative URL for a particular 'page' number.
+
+    Args:
+        value: The position within the paginated QuerySet. Often an integer but
+            can also be the string 'last.'
+        field_name: The name of the parameter containing the position value.
+            Most often 'page' but can be overridden.
+        urlencode: Encoded GET parameters (if any). Will contain the position
+            parameter if present in the query string.
+
+    Returns:
+        A relative URL beginning with ?<field_name>=<value>. Any other
+            parameters will be appended.
+    """
+    url = '?{}={}'.format(field_name, value)
+
+    if urlencode:
+        # Only include parameters that don't match field_name.
+        qs = urlencode.split('&')
+        filtered_qs = filter(lambda p: p.split('=')[0] != field_name, qs)
+        encoded_qs = '&'.join(filtered_qs)
+
+        if encoded_qs:
+            url = '{}&{}'.format(url, encoded_qs)
+
+    return url
