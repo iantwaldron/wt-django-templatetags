140720ecd30d5b5abad152ccf8ea78656c0808f3
Ian Waldron <ian@waldrontech.com>
2025-09-18 10:49:06 -0700

    Add tests dir and add test settings module.

---
+++ tests/test_settings.py
@@ -0,0 +1,31 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+SECRET_KEY = 'secretkey'
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'wt_tools'
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory:'}}
+
+USE_TZ = True
