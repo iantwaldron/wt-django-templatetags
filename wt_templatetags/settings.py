2719b9fcbcd36e05c6196e170f2738983e7fdcbf
Ian Waldron <ian@waldrontech.com>
2025-10-20 09:12:51 -0700

    Update settings for an app_settings object approach like drf.

--- wt_templatetags/settings.py
+++ wt_templatetags/settings.py
@@ -2,13 +2,71 @@ from django.conf import settings
 
 # TODO: Implement validation system checks
 
-# validate is sequence
-# validate items are string
-# validate char first position is dot
-STATIC_MIN_EXTENSIONS = getattr(settings, 'WT_STATIC_MIN_EXTENSIONS', ['.css', '.js'])
+DEFAULTS = {
+    # validate is sequence
+    # validate items are string
+    # validate char first position is dot
+    'STATIC_MIN_EXTENSIONS': ['.css', '.js'],
+    # validation is string
+    'STATIC_MIN_SUFFIX': 'min',
+    # validate is bool
+    'STATIC_MIN_FAIL_SILENT': True
+}
 
-# validation is string
-STATIC_MIN_SUFFIX = getattr(settings, 'WT_STATIC_MIN_SUFFIX', 'min')
 
-# validate is bool
-STATIC_MIN_FAIL_SILENT = getattr(settings, 'WT_STATIC_MIN_FAIL_SILENT', True)
+class AppSettings:
+    """
+    Settings for wt_django_templatetags are all namespaced in the
+       WT_TEMPLATETAGS setting. This module provides the 'api_setting'
+       object that's used to access app settings. User settings are checked
+       and defaults are used as a fallback if user settings aren't found.
+
+    """
+
+    # The format of this settings object follows the drf approach.
+
+    def __init__(self, defaults=None, user_settings=None):
+        self.defaults = defaults or DEFAULTS
+        if user_settings:
+            self._user_settings = user_settings
+        # for reload method
+        self._cached_attrs = set()
+
+    @property
+    def user_settings(self):
+        if not hasattr(self, '_user_settings'):
+            self._user_settings = getattr(settings, 'WT_TEMPLATETAGS', {})
+        return self._user_settings
+
+    def __getattr__(self, attr):
+        # called when an AttributeError is raised
+
+        if not attr in self.defaults:
+            # check defaults first
+            raise AttributeError(f"Invalid setting: {attr}.")
+
+        try:
+            # check first if there's a user setting
+            val = self.user_settings[attr]
+        except KeyError:
+            # use defaults if not in user_settings
+            val = self.defaults[attr]
+
+        # remember cached attributes for reload
+        self._cached_attrs.add(attr)
+        # now cache the result
+        setattr(self, attr, val)
+        return val
+
+    def reload(self):
+        for attr in self._cached_attrs:
+            # only remove attrs that have been cached
+            delattr(self, attr)
+        # now clear the set to start over
+        self._cached_attrs.clear()
+
+        if hasattr(self, '_user_settings'):
+            delattr(self, '_user_settings')
+
+
+app_settings = AppSettings(None, DEFAULTS)
